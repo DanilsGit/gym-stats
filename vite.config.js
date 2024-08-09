003e27bf4bae63eb047fe21d7818e6337075c908
DanilsGit <danielestebanmarquez12@gmail.com>
2024-08-08 23:51:04 -0500

    Config PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),VitePWA({
+    manifest: {
+      name: "Gym stats",
+      short_name: "Gym stats | Crea, personaliza y comparte tus rutinas",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#10283d",
+      theme_color: "#1a252f",
+      icons : [
+        {
+          src: "./logo.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
+    }
+  })],
   define: {
     'global': {}
   },
